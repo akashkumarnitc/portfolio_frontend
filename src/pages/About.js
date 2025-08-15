ab8cac86995a794c9f097a21e043a7971d11c564
AKASH KUMAR <133356534+akashkumarnitc@users.noreply.github.com>
2025-08-16 04:51:46 +0530

    Update About.js

--- src/pages/About.js
+++ src/pages/About.js
@@ -6,7 +6,8 @@ const About = () => {
     <Container>
       <Typography variant="h3" gutterBottom>About Me</Typography>
       <Typography variant="body1">
-      Hi, I’m Akash Kumar, a passionate Computer Science student eager to solve real-world problems through technology. I specialize in full-stack development with Django and React, crafting efficient and scalable applications. My expertise spans machine learning, computer networking, and software engineering, with hands-on experience in CNN-based image classification and system design. I thrive on building innovative solutions, from exam management systems to face ID-based attendance tracking. Always keen to learn and grow, I aim to contribute meaningfully to the tech industry. Let’s connect and create something impactful!
+      Hi, I’m Akash Kumar, a passionate Computer Science student eager to solve real-world problems through technology. I specialize in full-stack development with Django and React, crafting efficient and scalable applications. My expertise spans machine learning, computer networking, and software engineering, with hands-on experience in CNN-based image classification and system design. I thrive on building innovative solutions, from exam management systems to face ID-based attendance tracking. Always keen to learn and grow, I aim to contribute meaningfully to the tech industry. Let’s connect and create something impactful! now i am thinking of renovating this page!!
+    
       </Typography>
     </Container>
   );
